5f5142c28d438d5be84efca59bdbcc4dca6f3a58
chirania-kunal <chiraniakunal@gmail.com>
2024-11-29 11:50:40 +0530

    Adding display box

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import "./App.css";
-import { useState } from "react";
+import { useState,useRef } from "react";
 
 function App() {
 
   const [count, setCount] = useState(0);
+  const divRef = useRef(null);
+
   
   function decrementHandler(){
+    // let value=count;
     setCount(count-1);
+    divRef.current.textContent=count+" was decremented, and it becomes "+ (count-1);
+
   }
 
   function incrementHandler(){
+    let value=count;
     setCount(count+1);
+    divRef.current.textContent=value+" was incremented, and it becomes "+ (value+1);
+
   }
   
   function ResetHandler(){
     setCount(0);
+    divRef.current.textContent="You have reset it."
   }
+
 
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
@@ -33,6 +43,9 @@ function App() {
         </div>
         <div>
           <button onClick={ResetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg"> Reset </button>
+        </div>
+        <div id="displaybox"  class="w-72 h-52 border-2 border-black rounded-lg bg-blue-200 p-5 text-center text-gray-800">
+            <h2 id="box" ref={divRef} className="text-2xl">Start</h2>
         </div>
 
     </div>
